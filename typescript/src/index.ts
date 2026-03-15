da8bc390105043f3b93ab20090f01d966e09269c
Karolina Queiroz <fkdnq.lic21@uea.edu.br>
2026-03-15 17:08:22 -0400

    Entendendo a diferença entre type e interface

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,27 +1,20 @@
-interface Product{
+type Category ={
+  name:string
+}
+type Product ={
   name:string,
   price:number,
   description:string,
   isActive:boolean,
+  category:Category
 }
-interface Product{
-  category:string
-}
-
-type ProductType ={
-  name:string,
-  price:number,
-  description:string,
-  isActive:boolean
-}
-
 
 const product:Product = {
   name: "teste",
   price: 20.2,
   description:"description",
   isActive: true,
-  category:"futebol"
-};
-
-product.price
+  category:{  //objeto
+    name:"futebol"
+  }
+}
