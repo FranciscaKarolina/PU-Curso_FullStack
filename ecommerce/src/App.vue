80ba3dfe9f652a7462ee2b316b278b0201aaa290
Karolina Queiroz <fkdnq.lic21@uea.edu.br>
2026-03-21 09:32:32 -0400

    Inserindo produtos no carrinho

--- ecommerce/src/App.vue
+++ ecommerce/src/App.vue
@@ -5,6 +5,12 @@ export default {
         return{
             cart:{
                 total:0
+            },
+            product:{
+                title: "Guitarra",
+                description:"22 trastes em aço",
+                price:20,
+                discount:0.2
             }
 
         }
@@ -23,6 +29,18 @@ export default {
 
 <template>
     <main>
+        <article>
+            <h1>{{ product.title}}</h1>
+            <p>{{ product.description }}</p>
+            <p>R$:{{product.price.toFixed(2).replace(',', ',')}}</p>
+            <p>total:{{ cart.total + 1}}</p>
+        </article>
+        <button @click="addItem()">Adicionar</button>
+        <button @click="decItem()">Decrementar</button>
+
+
+
+
         <article>
             <h1>Carrinho</h1>
             <p>total:{{ cart.total + 1}}</p>
